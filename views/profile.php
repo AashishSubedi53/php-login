23e06aee5d9e2fe830e2497b60c1f7c8f467b1ef
BIpin <bipin.regmi.s21@gmail.com>
2023-10-11 11:36:03 +0545

    Bipin Change

--- views/profile.php
+++ views/profile.php
@@ -1,15 +1,15 @@
 <?php
 
 require '../vendor/autoload.php';
+
 use Asis\OopProject\Model\User;
 use Asis\OopProject\Controller\LoginController;
 
-if($_GET){
-    if(isset($_GET["e"])){
+if ($_GET) {
+    if (isset($_GET["e"])) {
         $encodedEmail = $_GET["e"];
 
         $email = base64_decode($encodedEmail);
-        
     }
 }
 
@@ -32,7 +32,7 @@ $users = $user->all($email);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/profile.css">
-    <title>Profile Page</title>
+    <title>Profile Bipin Page</title>
 </head>
 
 <body>
@@ -43,33 +43,32 @@ $users = $user->all($email);
                     <div class="card-body">
                         <h2 class="card-title text-center">User Profile</h2>
                         <hr>
-                        <?php foreach($users as $user){ ?>
-                        <div class="text-center">
-                            <img src="<?php echo $user['profile_image_url']; ?>" alt="Profile Image"
-                                class="rounded-circle img-thumbnail" width="150">
-                        </div>
+                        <?php foreach ($users as $user) { ?>
+                            <div class="text-center">
+                                <img src="<?php echo $user['profile_image_url']; ?>" alt="Profile Image" class="rounded-circle img-thumbnail" width="150">
+                            </div>
 
-                        <div class="text-center mt-3">
-                            <h4><?php echo $user['name']; ?></h4>
-                            <p><?php echo $user['email']; ?></p>
-                        </div>
-                        <div class="mt-4">
-                            <h5>User Information</h5>
-                            <ul class="list-group">
+                            <div class="text-center mt-3">
+                                <h4><?php echo $user['name']; ?></h4>
+                                <p><?php echo $user['email']; ?></p>
+                            </div>
+                            <div class="mt-4">
+                                <h5>User Information</h5>
+                                <ul class="list-group">
 
-                                <li class="list-group-item"><strong>Name:</strong> <?php echo $user['name']; ?></li>
-                                <li class="list-group-item"><strong>Email:</strong> <?php echo $user['email']; ?></li>
+                                    <li class="list-group-item"><strong>Name:</strong> <?php echo $user['name']; ?></li>
+                                    <li class="list-group-item"><strong>Email:</strong> <?php echo $user['email']; ?></li>
 
-                                <li class="list-group-item"><strong>Age:</strong> <?php echo $user['age']; ?></li>
-                                <li class="list-group-item"><strong>Address:</strong> <?php echo $user['address']; ?>
-                                </li>
-                                <li class="list-group-item"><strong>Gender:</strong>
-                                    <?php echo $user['gender'] ? 'Male' : 'Female'; ?></li>
+                                    <li class="list-group-item"><strong>Age:</strong> <?php echo $user['age']; ?></li>
+                                    <li class="list-group-item"><strong>Address:</strong> <?php echo $user['address']; ?>
+                                    </li>
+                                    <li class="list-group-item"><strong>Gender:</strong>
+                                        <?php echo $user['gender'] ? 'Male' : 'Female'; ?></li>
 
 
-                            </ul>
-                            <?php }?>
-                        </div>
+                                </ul>
+                            <?php } ?>
+                            </div>
                     </div>
                 </div>
             </div>

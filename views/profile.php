dcebeeb858f602330fb4bf34a865ad37dccd149d
AshishSubedi53 <ashishsubedi53@gmail.com>
2023-10-11 11:35:23 +0545

    profile second change

--- views/profile.php
+++ views/profile.php
@@ -32,7 +32,7 @@ $users = $user->all($email);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/profile.css">
-    <title>Profile Page</title>
+    <title>Profile Ashish Page</title>
 </head>
 
 <body>
